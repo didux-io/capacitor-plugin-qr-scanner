a6d1b001bd337f397edcf0598a608eb5f3eba0ae
Daniel Leushuis <danieleushuis@gmail.com>
2022-02-17 10:54:16 +0100

    init

---
+++ src/web.ts
@@ -0,0 +1,48 @@
+import { WebPlugin } from '@capacitor/core';
+
+import type {
+  BarcodeScannerPlugin,
+  ScanOptions,
+  ScanResult,
+  CheckPermissionOptions,
+  CheckPermissionResult,
+  StopScanOptions,
+} from './definitions';
+
+export class BarcodeScannerWeb
+  extends WebPlugin
+  implements BarcodeScannerPlugin {
+  async prepare(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async hideBackground(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async showBackground(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async startScan(_options: ScanOptions): Promise<ScanResult> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async startScanGif(_options: ScanOptions): Promise<ScanResult> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async stopScan(_options?: StopScanOptions): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async checkPermission(
+    _options: CheckPermissionOptions,
+  ): Promise<CheckPermissionResult> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+
+  async openAppSettings(): Promise<void> {
+    throw this.unimplemented('Not implemented on web.');
+  }
+}
